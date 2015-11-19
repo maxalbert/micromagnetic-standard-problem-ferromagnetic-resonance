f7a4691235cde58259963ae9c10cd6ff98cd3175
Maximilian Albert <maximilian.albert@gmail.com>
2015-11-19 16:29:25 +0000

    Image comparison tests for all figures pass now. Time to start refactoring...

--- src/tests/test_reproduce_figures.py
+++ src/tests/test_reproduce_figures.py
@@ -1,8 +1,25 @@
 import sys; sys.path.insert(0, '..')
 from matplotlib.testing.decorators import image_comparison
-from postprocessing import figure2
+from postprocessing import figure2, figure3, figure4_and_5
 
 
 @image_comparison(baseline_images=['figure2_OOMMF'], extensions=['png', 'pdf'])
 def test_reproduce_figure_2():
     figure2("../../data/oommf/dynamic_txyz.txt", software='OOMMF')
+
+
+@image_comparison(baseline_images=['figure3_OOMMF'], extensions=['png', 'pdf'])
+def test_reproduce_figure_3():
+    figure3("../../data/oommf/dynamic_txyz.txt", "../../data/oommf/mys_ft_abs.npy", software='OOMMF')
+
+
+@image_comparison(baseline_images=['figure4_OOMMF', 'figure5_OOMMF'], extensions=['png', 'pdf'])
+def test_reproduce_figure_4_and_5():
+    figure4_and_5("../../data/oommf/dynamic_txyz.txt",
+                  "../../data/oommf/mxs_ft_abs.npy",
+                  "../../data/oommf/mys_ft_abs.npy",
+                  "../../data/oommf/mzs_ft_abs.npy",
+                  "../../data/oommf/mxs_ft_phase.npy",
+                  "../../data/oommf/mys_ft_phase.npy",
+                  "../../data/oommf/mzs_ft_phase.npy",
+                  software='OOMMF')
