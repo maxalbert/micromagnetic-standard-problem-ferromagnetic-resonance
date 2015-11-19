c0cc6e4ea01f9f701d6d402603e0626c5f290fa2
Maximilian Albert <maximilian.albert@gmail.com>
2015-11-19 12:44:20 +0000

    Use fake sample data for test.

--- src/tests/test_data_loader.py
+++ src/tests/test_data_loader.py
@@ -6,9 +6,9 @@ from data_loader import DataLoader
 class TestDataLoader(object):
     @classmethod
     def setup_class(cls):
-        cls.data_loader = DataLoader('../../data/oommf', software='OOMMF')
+        cls.data_loader = DataLoader('./sample_data/oommf', software='OOMMF')
 
     def test_get_timesteps(self):
         ts = self.data_loader.get_timesteps()
-        ts_expected = np.linspace(0, 20e-9, 4000)
+        ts_expected = [0, 5e-12, 1e-11, 1.5e-11, 2e-11, 2.5e-11]
         assert np.allclose(ts, ts_expected)
