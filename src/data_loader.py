590b449f257670dc13b72806416ba92158a9d9cf
Maximilian Albert <maximilian.albert@gmail.com>
2015-11-19 13:09:56 +0000

    Add methods to obtain the x/y/z component of the spatially averaged magnetisation.

--- src/data_loader.py
+++ src/data_loader.py
@@ -13,12 +13,36 @@ class DataLoader(object):
         self.software = software
         assert self.software in ['OOMMF', 'Nmag']
 
+        data_avg_filename = os.path.join(self.data_dir, 'dynamic_txyz.txt')
+        self.data_avg = np.loadtxt(data_avg_filename)
+
     def get_timesteps(self):
         """
         Return a 1D numpy array containing the timesteps at which
         which the magnetisation was saved during the simulation.
         """
-        filename = os.path.join(self.data_dir, 'dynamic_txyz.txt')
-        data_avg = np.loadtxt(filename)
-        ts = data_avg[:, 0]
-        return ts
+        # Timestamps are contained in the first column of the averaged data
+        return self.data_avg[:, 0]
+
+    @staticmethod
+    def _get_index_of_m_avg_component(component):
+        """
+        Internal helper function to return the column index for
+        the x/y/z component of the average magnetisation.
+        """
+        try:
+            idx = {'x': 1, 'y': 2, 'z': 3}[component]
+        except IndexError:
+            raise ValueError(
+                "Argument 'component' must be one of 'x', 'y', 'z'. "
+                "Got: '{}'".format(component))
+        return idx
+
+    def get_average_magnetisation(self, component):
+        """
+        Return a 1D numpy array containing the values of the
+        spatially averaged magnetization sampled at the time-
+        steps during the simulation.
+        """
+        idx = self._get_index_of_m_avg_component(component)
+        return self.data_avg[:, idx]
--- src/tests/test_data_loader.py
+++ src/tests/test_data_loader.py
@@ -12,3 +12,13 @@ class TestDataLoader(object):
         ts = self.data_loader.get_timesteps()
         ts_expected = [0, 5e-12, 1e-11, 1.5e-11, 2e-11, 2.5e-11]
         assert np.allclose(ts, ts_expected)
+
+    def test_get_average_magnetisation(self):
+        mys = self.data_loader.get_average_magnetisation(component='y')
+        mzs = self.data_loader.get_average_magnetisation(component='z')
+
+        mys_expected = [0, -0.1, -0.2, -0.3, -0.4, -0.5]
+        mzs_expected = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5]
+
+        np.allclose(mys, mys_expected)
+        np.allclose(mzs, mzs_expected)
