9e362ce57312bbb37b63d8dc5c0bb0db66d4017e
Maximilian Albert <maximilian.albert@gmail.com>
2015-11-25 22:36:05 +0000

    Minor simplification.

--- src/eigenmode_plotter.py
+++ src/eigenmode_plotter.py
@@ -53,16 +53,14 @@ class EigenmodePlotter(object):
         self.shape = (24, 24)  # FIXME: this class should not need to know about this
 
     @staticmethod
-    def plot_mode_component(fig, gs, data, label, vmin, vmax, cmap):
-        ax = fig.add_subplot(gs)
+    def plot_mode_component(ax, data, label, vmin, vmax, cmap):
         ax.imshow(data, cmap=cmap, vmin=vmin, vmax=vmax, origin='lower')
         ax.set_title(label)
         ax.set_xticks([])
         ax.set_yticks([])
 
     @staticmethod
-    def plot_colorbar(fig, gs, label, cmap, vmin, vmax, num_ticks, ticklabels=None):
-        ax = fig.add_subplot(gs)
+    def plot_colorbar(ax, label, cmap, vmin, vmax, num_ticks, ticklabels=None):
         norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
         ticks = np.linspace(vmin, vmax, num_ticks)
         cbar = mpl.colorbar.ColorbarBase(
@@ -77,10 +75,9 @@ class EigenmodePlotter(object):
         and phases.
         """
         fig = plt.figure(figsize=(8, 6))
-        gs = gridspec.GridSpec(2, 4,
-                          width_ratios=[4, 4, 4, 0.5], height_ratios=[4, 4])
-
-        components = ['x', 'y', 'z']
+        gs = gridspec.GridSpec(2, 4, width_ratios=[4, 4, 4, 0.5],
+                                     height_ratios=[4, 4])
+        axes = [fig.add_subplot(g) for g in gs]
 
         amp_x = self.data_reader.get_mode_amplitudes(freq, 'x').reshape(self.shape)
         amp_y = self.data_reader.get_mode_amplitudes(freq, 'y').reshape(self.shape)
@@ -94,15 +91,15 @@ class EigenmodePlotter(object):
         minVal = np.min([amp_x, amp_y, amp_z])
         maxVal = np.max([amp_x, amp_y, amp_z])
 
-        self.plot_mode_component(fig, gs[0], amp_x, 'x', cmap=self.cmap_amplitude, vmin=minVal, vmax=maxVal)
-        self.plot_mode_component(fig, gs[1], amp_y, 'y', cmap=self.cmap_amplitude, vmin=minVal, vmax=maxVal)
-        self.plot_mode_component(fig, gs[2], amp_z, 'z', cmap=self.cmap_amplitude, vmin=minVal, vmax=maxVal)
-        self.plot_colorbar(fig, gs[3], 'Amplitude', self.cmap_amplitude, vmin=0, vmax=maxVal, num_ticks=5)
+        self.plot_mode_component(axes[0], amp_x, 'x', cmap=self.cmap_amplitude, vmin=minVal, vmax=maxVal)
+        self.plot_mode_component(axes[1], amp_y, 'y', cmap=self.cmap_amplitude, vmin=minVal, vmax=maxVal)
+        self.plot_mode_component(axes[2], amp_z, 'z', cmap=self.cmap_amplitude, vmin=minVal, vmax=maxVal)
+        self.plot_colorbar(axes[3], 'Amplitude', self.cmap_amplitude, vmin=0, vmax=maxVal, num_ticks=5)
 
-        self.plot_mode_component(fig, gs[4], phase_x, 'x', cmap=self.cmap_phase, vmin=-np.pi, vmax=+np.pi)
-        self.plot_mode_component(fig, gs[5], phase_y, 'y', cmap=self.cmap_phase, vmin=-np.pi, vmax=+np.pi)
-        self.plot_mode_component(fig, gs[6], phase_z, 'z', cmap=self.cmap_phase, vmin=-np.pi, vmax=+np.pi)
-        self.plot_colorbar(fig, gs[7], 'Phase', self.cmap_phase, vmin=-np.pi, vmax=np.pi, num_ticks=3, ticklabels=['-3.14', '0', '-3.14'])
+        self.plot_mode_component(axes[4], phase_x, 'x', cmap=self.cmap_phase, vmin=-np.pi, vmax=+np.pi)
+        self.plot_mode_component(axes[5], phase_y, 'y', cmap=self.cmap_phase, vmin=-np.pi, vmax=+np.pi)
+        self.plot_mode_component(axes[6], phase_z, 'z', cmap=self.cmap_phase, vmin=-np.pi, vmax=+np.pi)
+        self.plot_colorbar(axes[7], 'Phase', self.cmap_phase, vmin=-np.pi, vmax=np.pi, num_ticks=3, ticklabels=['-3.14', '0', '-3.14'])
 
         fig.subplots_adjust(left=0.1, bottom=0.1, right=0.95, wspace=0.1)
         fig.suptitle('{:.2f} GHz'.format(freq * 1e-9), fontsize=20)
